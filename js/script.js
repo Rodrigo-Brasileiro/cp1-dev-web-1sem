f6c8390e47e190d3f153e1e5faa5f6884eb2de40
MigsRS123 <rm551239@fiap.com.br>
2023-04-04 18:04:23 -0300

    Exercícios 7 e 8 !

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// 7 -Crie um botão em HTML e utilize o evento de clique em Javascript para alterar a cor de fundo da página para verde.
+//  Para isso, utilize o código document.body.style.backgroundColor = 'green';dentro da função do evento de clique.
+// Exemplifique e demonstre a saída!(20Pontos)
+
 // const h1Element = document.getElementById("botao");
 // h1Element.addEventListener("click",()=>{
 //     alert('Mundando a cor')
@@ -11,7 +15,11 @@
 
 
 
-
+// 8 -Crieum programa em Javascript que contenha um evento em que, ao passar o mouse sobre uma div, o texto e a cor de fundo da div
+//  são alterados. O aluno deverá criar a estrutura do evento e declarar o id da div no HTML.
+//   Dica: para alterar o texto e a cor de fundoda div, utilize o método innerHTML para modificar o  conteúdo  da  div  e  a  
+//   propriedade  style.backgroundColor  para  alterar  a  cor  de  fundo. 
+// Exemplifique  e demonstre a saída!(20Pontos)
 
 // const h1Element = document.getElementById("identidade");
 
