c8c0b4a5f7e18c223e7b2827cf8d56b34c491e0e
MigsRS123 <rm551239@fiap.com.br>
2023-04-04 18:00:53 -0300

    Exercicios 7 e 8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-const h1Element = document.getElementById("botao");
-h1Element.addEventListener("click",()=>{
-    alert('Mundando a cor')
-    h1Element.style.backgroundColor = 'green';
-})
+// const h1Element = document.getElementById("botao");
+// h1Element.addEventListener("click",()=>{
+//     alert('Mundando a cor')
+//     h1Element.style.backgroundColor = 'green';
+// })
+
+
+
+
+
+
+
+
+
+
+// const h1Element = document.getElementById("identidade");
+
+// h1Element.addEventListener("mouseover",()=>{
+//     h1Element.style.backgroundColor = "#152366";
+//     h1Element.style.color = "#fff";
+// })
+
+// h1Element.addEventListener("mouseout",()=>{
+//     h1Element.style.backgroundColor = "#fff";
+//     h1Element.style.color = "#000000";
+// })
