bf99b58c4ed60ea8093b30093bf13f2186a2567b
Rodrigo.Brasileiro <rm98952@fiap.com.br>
2023-04-04 18:41:17 -0300

    exercício 3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-a  = 5%2
-console.log(a)
+var peso = prompt("Digite seu peso em kg:");
+
+var altura = prompt("Digite sua altura em m:");
+
+var imc = peso/(altura**2)
+
+if ( 18.5 > imc )
+
+{
+        console.log("Você está abaixo do peso")
+}
+
+else if (imc > 24.9 )
+{
+        console.log("Você está acima do peso")
+} 
+
+else {
+        console.log("Você está no peso ideal")
+}
+    console.log(imc)
