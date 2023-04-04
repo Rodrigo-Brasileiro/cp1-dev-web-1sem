2c780935e518d7484dd197e5b9642e9f28180c97
ricardoakira <rm551447@fiap.com.br>
2023-04-04 17:29:09 -0300

    exercicio 6

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-let variavel
-console.log(variavel)
+var usuario = prompt("Nome de usuário: ");
+var senha = prompt("Senha: ")
+if (usuario == "admin" && senha == "1234"){
+    console.log("Login foi realizado com sucesso")
+}
+else{
+    console.log("Houve falha na autenticação")
+}
