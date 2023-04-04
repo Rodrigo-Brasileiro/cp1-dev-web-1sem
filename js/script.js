f8fc1ad08a5c2e0fde164be2c278037fe1ddbd1f
MigsRS123 <rm551239@fiap.com.br>
2023-04-04 17:37:48 -0300

    Questão 7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-let variável
-console.log(variável)
-//TENTATIVA
+const h1Element = document.getElementById("botao");
+h1Element.addEventListener("click",()=>{
+    alert('Mundando a cor')
+    h1Element.style.backgroundColor = 'green';
+})
