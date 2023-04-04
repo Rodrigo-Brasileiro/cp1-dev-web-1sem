f0586ecc7b401cbb3fcf9a511732784e8d0c50af
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-04-04 18:19:01 -0300

    Construção do exercício 4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 // 4 - Faça um programa que recebaa idade de uma pessoa e exibauma mensagem indicando em qual faixa etária ela se encontra: criança (até 12 anos), adolescente (entre 13 e 18 anos), adulto (entre 19 e60 anos) ou idoso (mais de 60 anos).Exemplifique e demonstre a saída!
 
-var idade = prompt("Escolher uma idade de 1 a 100: ");
+// var idade = prompt("Escolher uma idade de 1 a 100: ");
+
+
+if (idade <= 12){
+     console.log("Você é criança")
+}     
+else if (idade >= 13 && idade <= 18){  
+     console.log("Você é adolescente")
+}     
+else if (idade >= 19 && idade <= 60){ 
+     console.log("Você é adulto")
+}     
+else{
+     console.log("Você é idoso")
+}     
+
 
