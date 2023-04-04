ebbb3cdd6fba998fbb627c34979451139544a90c
lucasvital22 <rm551040@fiap.com>
2023-04-04 17:22:25 -0300

    ex6

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-let variável
-console.log(variável)
+// 6 -Vamoscriar um programa que calculea média de 7notas
+//    e exibauma mensagem indicando se o aluno foi aprovado ou reprovado. 
+//    Considere que a média mínima para aprovação é 6.Exemplifique e demonstre a saída!
+
+//  let n1= parseFloat(prompt("informe sua primeira nota: "))
+//  let n2= parseFloat(prompt("informe sua segunda nota: "))
+//  let n3= parseFloat(prompt("informe sua terceira nota: "))
+//  let n4= parseFloat(prompt("informe sua quarta nota: "))
+//  let n5= parseFloat(prompt("informe sua quinta nota: "))
+//  let n6= parseFloat(prompt("informe sua sexta nota: "))
+//  let n7= parseFloat(prompt("informe sua setima nota: "))
+
+// let media=(n1 + n2 + n3 + n4 + n5 + n6 + n7)/7;
+
+//  if (media >= 6) {
+//    console.log("Parabéns!!! Você foi aprovado :) , sua média foi de " + media.toFixed(2));
+//  } else {
+//    console.log("Você foi reprovado :( , sua média foi de " + media.toFixed(2));
+//  }
